4ef2fbde375decad5a04598fd9cfff06033ccbc5
zenlp <ml654321@hotmail.com>
2018-05-18 13:00:35 +1000

    Test another GitHub checkin

--- TestConsoleApp2/Program.cs
+++ TestConsoleApp2/Program.cs
@@ -6,7 +6,7 @@ namespace TestConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World GitHub!");
         }
     }
 }
